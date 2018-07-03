efaabe632113e4ab53cd4d0458095d8f05152e2a
Thread <thread@mail.com>
2018-07-03 16:20:19 +0300

    master.js

---
+++ js/master.js
@@ -0,0 +1,17 @@
+// this function includes all necessary js files for the application
+function include(file) {
+    var script   = document.createElement('script');
+    script.src   = file;
+    script.type  = 'text/javascript';
+    script.defer = true;
+
+    document.getElementsByTagName('head').item(0).appendChild(script);
+    //document.body.insertAdjacentHTML('beforeEnd','script');
+    //document.body.appendChild(script);
+}
+
+// include any js files here
+include('js/less.min.js');
+include('js/prefixes.js');
+include('js/jquery.min.js');
+include('js/jquery.cookie.js');
